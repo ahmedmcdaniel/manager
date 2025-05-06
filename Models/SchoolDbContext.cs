7ba26ac73e6d557c599a581d2d241b311229a7d1
ahmedmcdaniel <ahmedsito8787@gmail.com>
2025-05-05 21:50:07 -0500

    dddd

--- Models/SchoolDbContext.cs
+++ Models/SchoolDbContext.cs
@@ -57,7 +57,7 @@ public partial class SchoolDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Database=SchoolManagement;Username=postgres;Password=Panama2020$");
+        => optionsBuilder.UseNpgsql("Host=dpg-d0cmkdqdbo4c73fkak7g-a;Port=5432;Database=schoolmanager_32b8;Username=schoolmanager_32b8_user;Password=0HR1DJuSoiYDgZqVBzXVIRHeClEW8MaY;SSL Mode=Require;Trust Server Certificate=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
